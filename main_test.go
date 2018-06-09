d81e0f7e1cdf9c00159123bfd3c50b5afaafe7c1
Leonidas <lmaroulis2003@gmail.com>
2018-06-09 08:24:51 +0300

    fix travis

--- main_test.go
+++ main_test.go
@@ -19,7 +19,7 @@ func TestMain(t *testing.T) {
 		Timeout: 1 * time.Second,
 	}
 
-	r, _ := http.NewRequest("GET", "http://localhost:8081/", nil)
+	r, _ := http.NewRequest("GET", "http://127.0.0.1:8081/", nil)
 
 	resp, err := client.Do(r)
 	if err != nil {
